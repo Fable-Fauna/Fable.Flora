68926e1444ca79060251f4692935c679813a8d0c
orlando <musheddev@users.noreply.github.com>
2019-04-24 17:38:48 -0500

    begin testing of new tokenizer

--- src/Flora.CssParser/Tokenizer.fs
+++ src/Flora.CssParser/Tokenizer.fs
@@ -6,16 +6,45 @@ open System.Text.RegularExpressions
 
 //https://www.w3.org/TR/css-syntax-3/
 
+type Parse<'T> = 
+    { Comments : string list 
+      Errors : string list
+      Result : 'T option}
+    
+type Parser<'T> = string -> Parse<'T> * string
+
+let (.>>) (p1 : Parser<'a>) (p2 : Parser<'b>) = 
+    fun str -> 
+        match (p1 str) with
+        | (R1,left) when R1.Result.IsSome ->
+            match (p2 left) with
+            | (R2,left) when R2.Result.IsSome ->
+                {R1 with Comments = R1.Comments @ R2.Comments},left
+            | (R2,left) -> 
+                {Comments = R1.Comments @ R2.Comments
+                 Errors = R1.Errors @ R2.Errors
+                 Result = None}, left
+        | x -> x
+                   
+
+
+type Consumer = Parser<unit>
+
+type NumberToken =
+    { Exponent : (bool * string) option
+      Number : (bool * string) option
+      Decimal : string option }
+
 [<RequireQualifiedAccessAttribute>]
 type Token =
     | Whitespace of string list
     | Ident of string
-    | Function
-    | At
+    | Function of string
+    | At of string
     | Hash of string
     | String of string
-    | Url
-    | Number 
+    | Url of string
+    | Number of NumberToken
     | Dimension
     | Percentage
     | UnicodeRange
@@ -27,7 +56,7 @@ type Token =
     | Column
     | CDO
     | CDC
-    | Delim
+    | Delim of char
     | Colon
     | SemiColon
     | Comma
@@ -37,10 +66,35 @@ type Token =
     | SwiggleEnd
     | ParenStart
     | ParenEnd
+    
 
 
-let fixStream (str : string) =
-    str.Replace('\u000D','\u000A').Replace('\u000C','\u000A')
+// let fixStream (str : string) =
+//     str.Replace('\u000D','\u000A').Replace('\u000C','\u000A')
+
+let (|PChar|_|) (char : char) (input : string) =
+    match input with
+    | str when str.[0] = char -> Some(input.Remove(0,1))
+    | _ -> None
+
+let (|PString|_|) (str : string) (input : string) =
+    match input with
+    | str when str.StartsWith str -> 
+        Some(input.Remove(0,str.Length))
+    | _ -> None    
+
+let (|Between|_|) (char : char) (input : string) =
+    match input with
+    | PChar char str -> 
+        let i = str.IndexOf char
+        let s = str.Substring(0,i)
+        Some(s,input.Remove(0,i+1))
+    | _ -> None    
+
+let (|Char|_|) (input : string) =
+    if input.Length > 0 then
+        Some(input.[0],input.Remove(0,1)) 
+    else None    
 
 let (|NewLine|_|) (input : string) =
     if input.StartsWith("\u000D\u000A") then Some(input.Substring(2))
@@ -157,7 +211,8 @@ let (|IdentCodon|_|) (input : string) =
     | ary when ary.Length > 1 -> Some(c)
     | _ -> None
 
-let (|Ident|) (input : string) = 
+let (|Ident|_|) (input : string) = 
+    let mutable loop = true
     let mutable fst,rest =
         match input with
         | a when a.[0] = '-' -> 
@@ -166,21 +221,227 @@ let (|Ident|) (input : string) =
                 "-" + b, a.Remove(0,1)
             | Escape(b,left) ->
                 b, left
+            | _ -> failwith "bad id"
         | IdentCodon(a) -> 
             a,input.Remove(0,1)
         | Escape(a,left) ->
             a, left
-        | _ -> failwith "not ident"
-    let mutable loop = true
+        | _ -> loop <- false; "",""
+    if loop then
+        while loop do 
+            match rest with
+            | PChar '-' str -> 
+                fst <- fst + "-"
+                rest <- str
+            | IdentCodon(a) -> 
+                fst <- fst + a
+                rest <- rest.Remove(0,1)
+            | Escape(a,left) ->
+                fst <- fst + a
+                rest <- left
+            | _ -> loop <- false
+        Some(fst,rest)
+    else None
+
+let (|Function|_|) (input : string) =
+    match input with
+    | Ident(id,rest) ->
+        match rest with
+        | PChar '(' str -> Some(id,str)
+        | _ -> None 
+    | _ -> None
+
+
+let (|AtKeyword|_|)  (input : string) =
+    match input with
+    | PChar '@' (Ident(id,rest)) -> Some(id,rest)
+    | _ -> None
+
+let (|HashToken|_|) (input : string) =
+    match input with
+    | PChar '#' str -> 
+        let mutable loop = true
+        let mutable fst, rest = "",""
+        while loop do 
+            match rest with
+            | PChar '-' str -> 
+                fst <- fst + "-"
+                rest <- str
+            | IdentCodon(a) -> 
+                fst <- fst + a
+                rest <- rest.Remove(0,1)
+            | Escape(a,left) ->
+                fst <- fst + a
+                rest <- left
+            | _ -> loop <- false
+        Some(fst,rest)
+    | _ -> None
+
+let (|StringToken|_|) (input : string) = 
+    match input with
+    | Between '"' (str, left)
+    | Between ''' (str, left) ->
+        let mutable loop, result, rest  = true,"",str
+        while loop do 
+            match rest with
+            | Escape(a,left) ->
+                result <- result + a
+                rest <- left
+            | PChar '\\' (NewLine(left)) -> 
+                result <- result + "\u000A"
+                rest <- left
+            | Char (char, left) -> 
+                result <- result + string char
+                rest <- left
+            | _ -> ()
+        Some(result,left)    
+    | _ -> None
+
+let (|UrlUnQuote|_|) (input : string) =
+    let mutable loop, result, rest  = true,"",input
+    let mutable ret = false
     while loop do 
         match rest with
-        | a when a.[0] = '-' -> 
-            fst <- fst + "-"
-        | IdentCodon(a) -> 
-            fst <- fst + a
-            rest <- rest.Remove(0,1)
         | Escape(a,left) ->
-            fst <- fst + a
+            result <- result + a
             rest <- left
-        | _ -> loop <- false
-    Token.Ident(fst),rest
+        | Char (char, left) when 
+            char = '\\' 
+            || char = '"' 
+            || char = ''' 
+            || char = '('
+            || char = ')'
+            || Char.IsControl char
+            -> loop <- false //failwith "url cannot contain \\ \" ' ( ) "
+        | MaybeWhitespace(left) -> loop <- false
+        | Char (char,left) ->
+            result <- result + string char
+            rest <- left
+        | a -> 
+            result <- result + a
+            loop <- false
+            ret <- true
+    if ret then Some(result) else None        
+
+let (|UrlToken|_|) (input : string) =
+    match input with
+    | PString "url" (PChar '(' str) ->
+        let i = str.IndexOf ')'
+        let inner = str.Substring(0,i)
+        let rest = input.Remove(0,i+1)
+        match inner with
+        | MaybeWhitespace(_,left) ->
+            match left with
+            | UrlUnQuote(out) -> Some(out,rest)
+            | StringToken(out,MaybeWhitespace(_,left)) -> 
+                if left.Length > 0 then failwith "invliad url token"
+                Some(out,rest)
+            | _ -> None
+        | _ -> None
+    | _ -> None                
+
+let (|Digits|_|) (input : string) =
+    let mutable loop, result, rest  = true,"",input
+    while loop do 
+       match rest with
+       | PChar '0' left
+       | PChar '1' left
+       | PChar '2' left
+       | PChar '3' left
+       | PChar '4' left
+       | PChar '5' left
+       | PChar '6' left
+       | PChar '7' left
+       | PChar '8' left
+       | PChar '9' left ->
+            result <- result + string rest.[0]
+            rest <- left
+       | _ -> loop <- false   
+    if result.Length > 0 then
+        Some(result,rest)
+    else None     
+
+let (|NumberToken|_|) (input : string) =
+    let neg,left1 = 
+        match input with 
+        | PChar '+' left -> false,left
+        | PChar '-' left -> true,left
+        | str ->  false,str
+    let number, decimal, left2 = 
+        match left1 with
+        | Digits (num,(PChar '.' (Digits (d,left)))) -> num, d, left
+        | Digits (num,left) -> num, "", left
+        | PChar '.' (Digits (d,left)) -> "", d, left
+        | _ -> "", "", left1 
+    
+    let eneg, exponent, left3 =
+        match left2 with
+        | PChar 'e' left
+        | PChar 'E' left ->
+            match left with 
+            | PChar '+' (Digits (num,left)) -> false, num, left
+            | PChar '-' (Digits (num,left))-> true,num,left
+            | Digits (num,left) ->  false,num,left   
+        | _ -> false, "", left2 
+
+    if number = "" && decimal = "" then None 
+    else 
+    Some({ Exponent = if exponent = "" then None else Some(eneg,exponent) 
+           Number = if number = "" then None else Some(neg,number)
+           Decimal = if decimal = "" then None else Some(decimal)}, left3)
+
+
+let tokenise (input : string) =
+    match input with
+    | MaybeWhitespace(t,left) -> t,left
+    | StringToken(t,left) -> Token.String(t),left
+    | HashToken(t,left) -> Token.Hash(t),left //incorrect
+    | PString "$=" left -> Token.SuffixMatch,left
+    //apostrohpy string 
+    | PChar '(' left -> Token.ParenStart, left
+    | PChar ')' left -> Token.ParenEnd, left
+    | PString "*=" left -> Token.SubstringMatch, left
+    | NumberToken (num, left) -> Token.Number(num), left
+    | PChar ',' left -> Token.Comma, left
+    | PString "-->" left -> Token.CDC, left
+    //ident like
+    | Function (word,left) -> Token.Function word, left
+    | UrlToken (word,left) -> Token.Url word, left
+    | Ident (word,left) -> Token.Ident word, left
+    | PChar ':' left -> Token.Colon, left
+    | PChar ';' left -> Token.SemiColon, left
+    | PString "<!--" left -> Token.CDO, left
+    | AtKeyword (word,left) -> Token.At word, left
+    | PChar '[' left -> Token.SquareStart, left
+    //ident like
+    | PChar ']' left -> Token.SquareEnd, left
+    | PString "^=" left -> Token.PrefixMatch, left
+    | PChar '{' left -> Token.SwiggleStart, left
+    | PChar '}' left -> Token.SwiggleEnd, left
+    | PString "|=" left -> Token.DashMatch, left
+    | PString "||" left -> Token.Column, left
+    | PString "~=" left -> Token.IncludeMatch, left
+    //unicode range
+    //eof
+    | _ -> Token.Delim input.[0], input.Remove(0,1)
+
+type Stylesheet =
+    Rule list
+
+and Rule =
+    | Qualified of Component list * Block
+    | At of string * Component list * Block
+
+and Component =
+    | Preserved of Token
+    | SwiggleBlock of Block
+    | ParenBlock of Block
+    | SquareBlock of Block
+    | Function of string * Block
+
+and Block = Component list    
+
+let parse (input : Token list) =
+    match input with
+    | Token.Function(name) :: left -> left
+
--- src/UnitTests/TokenTests.fs
+++ src/UnitTests/TokenTests.fs
@@ -6,3 +6,17 @@ open Tokenizer
 open System.IO
 open Tests
 
+[<Theory>]
+[<InlineData("*")>]
+[<InlineData("")>]
+[<InlineData("|*")>]
+[<InlineData("*|*")>]
+[<InlineData("juice|*")>]
+[<InlineData("*|div")>]
+[<InlineData("juice|div")>]
+[<InlineData("|div")>]
+[<InlineData("div")>]
+let ``type selector`` (str : string) =
+    let result = run parseTypeSelector str
+    assert_success result
+
