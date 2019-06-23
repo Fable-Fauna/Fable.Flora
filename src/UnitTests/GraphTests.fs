0ef2a2a8ea35a002068bd4e6d138e984666fcde6
orlando <musheddev@users.noreply.github.com>
2019-06-22 20:34:18 -0500

    add graph tests

---
+++ src/UnitTests/GraphTests.fs
@@ -0,0 +1,29 @@
+﻿module GraphTests
+
+open CssProcesser
+open Expecto
+open System.IO
+
+[<Tests>]
+let AccepectenceTests =
+    testList "Graph Tests" [
+        test "tailwind" {
+
+            let g = makeGraphFromCss "../../../../../test/tailwind.css"
+            Expect.isTrue (g.Length > 1000) (sprintf "token length: %i"  g.Length)
+        };
+
+        test "bulma" {
+            let g = makeGraphFromCss "../../../../../test/bulma.css"
+           
+            Expect.isTrue (g.Length > 1000) (sprintf "token length: %i"  g.Length)
+        }
+
+        test "bootstrap" {
+            let g = makeGraphFromCss "../../../../../test/bootstrap.css"
+            
+            Expect.isTrue (g.Length > 1000) (sprintf "token length: %i"  g.Length)
+        }
+
+
+    ]
--- src/UnitTests/TokenTests.fs
+++ src/UnitTests/TokenTests.fs
@@ -10,43 +10,6 @@ open CssProvider.ParseShaper
 open CssProvider.SelectorsParser
 open Stream
 
-//[<Theory>]
-//[<InlineData("\"letters\"")>]
-//[<InlineData("'letters'")>]
-////[<InlineData("""""")>]
-////[<InlineData("""sbc""a""")>]
-//let ``string token`` (str : string) =
-//    match tokenStream str with
-//    | [Token.String(a)] -> true
-//    | _ -> false
-
-//[<Theory>]
-//[<InlineData("/* comment */")>]
-//[<InlineData(" ")>]
-//[<InlineData("  ")>]
-//let ``whitespace token`` (str : string) =
-//    match tokenStream str with
-//    | [Token.Whitespace(a)] -> true
-//    | _ -> false
-
-//[<Tests>]
-//let tests =
-//    testList "token tests" [
-//        test "string token" {
-//            match "\"letters\"" with
-//            | StringToken (str,left) -> Expect.equal str "letters" "string of letters"
-//        };
-
-//        test "pchar" {
-//            match "sa" with
-//            | PChar 's' left -> Expect.equal left "a" "pchar leaves rest"
-//        }
-
-//        test "between" {
-//            match "asa" with
-//            | Between 'a' (result,left) -> Expect.equal result "s" "between takes inside"; Expect.equal left "" "nothing left"
-//        }
-//    ]
 
 open CssProcesser
 [<Tests>]
